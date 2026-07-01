04e038a815eec56b33469524854f0762655f6df3
Aditi Singh <kumari.singh.aditi09@gmail.com>
2026-07-01 22:16:51 +0530

    Updated API URL

--- client/css/jss/images/js/products.js
+++ client/css/jss/images/js/products.js
@@ -1,75 +1,20 @@
-const products = [
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://e-commerce-product-catalog-vr9c.onrender.com")
+    .then((res) => res.json())
+    .then((data) => {
+      const container = document.getElementById("product-container");
 
-{
+      data.forEach((product) => {
+        const card = document.createElement("div");
 
-id:1,
+        card.innerHTML = `
+          <h3>${product.name}</h3>s
+          <p>₹${product.price}</p>
+          <p>${product.category}</p>
+        `;
 
-name:"Laptop",
-
-price:60000,
-
-category:"Electronics",
-
-image:"../images/laptop.jpg"
-
-},
-
-{
-
-id:2,
-
-name:"Shoes",
-
-price:2500,
-
-category:"Fashion",
-
-image:"../images/shoes.jpg"
-
-}
-
-];
-
-const container=document.getElementById("product-container");
-
-function displayProducts(productList){
-
-container.innerHTML="";
-
-productList.forEach(product=>{
-
-container.innerHTML+=`
-
-<div class="product-card">
-
-<img src="${product.image}" width="200">
-
-<h3>${product.name}</h3>
-
-<p>₹${product.price}</p>
-
-<p>${product.category}</p>
-
-<button onclick="addToCart(${product.id})">Add to Cart</button>
-
-</div>
-
-`;
-
+        container.appendChild(card);
+      });
+    })
+    .catch((err) => console.log(err));
 });
-
-}
-
-displayProducts(products);
-function addToCart(id) {
-
-    const selectedProduct = products.find(product => product.id === id);
-
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-    cart.push(selectedProduct);
-
-    localStorage.setItem("cart", JSON.stringify(cart));
-
-    alert(selectedProduct.name + " added to cart!");
-}
